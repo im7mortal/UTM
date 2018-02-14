a1f2cdcb82d07d2abddcb69bce73bcc468956429
sturfee-petrl <peterl@sturfee.com>
2018-02-14 13:57:09 -0800

    [] Clean benchmarks from deprecated API

--- utm_benchmark_test.go
+++ utm_benchmark_test.go
@@ -6,44 +6,34 @@ import (
 	"github.com/im7mortal/UTM"
 )
 
-var coordinate = UTM.Coordinate{466013, 7190568, 6, "W"}
+var coordinate = testCoordinate{466013, 7190568, 6, "W"}
 
 func BenchmarkToLatLon(b *testing.B) {
+	var err error
 	for i := 0; i < b.N; i++ {
-		if _, err := coordinate.ToLatLon(); err != nil {
+		if _, _, err = UTM.ToLatLon(coordinate.Easting, coordinate.Northing, coordinate.ZoneNumber, coordinate.ZoneLetter); err != nil {
 			b.Fatal("benchmark fatal BenchmarkToLatLon")
 		}
 	}
 }
 
 func BenchmarkToLatLonWithNorthern(b *testing.B) {
+	var err error
 	coordinate.ZoneLetter = ""
 	for i := 0; i < b.N; i++ {
-		if _, err := coordinate.ToLatLon(true); err != nil {
+		if _, _, err = UTM.ToLatLon(coordinate.Easting, coordinate.Northing, coordinate.ZoneNumber, coordinate.ZoneLetter, true); err != nil {
 			b.Fatal("benchmark fatal BenchmarkToLatLonWithNorthern")
 		}
 	}
 }
 
-var latLon = UTM.LatLon{64.83778, -147.71639}
+var latLon = testLatLon{64.83778, -147.71639}
 
 func BenchmarkFromLatLon(b *testing.B) {
+	var err error
 	for i := 0; i < b.N; i++ {
-		if _, err := latLon.FromLatLon(); err != nil {
+		if _, _, _, _, err = UTM.FromLatLon(latLon.Latitude, latLon.Longitude, false); err != nil {
 			b.Fatal("benchmark fatal BenchmarkFromLatLon")
 		}
-	}
-}
-
-func BenchmarkFromLatLonF(b *testing.B) {
-	defer func() {
-		if r := recover(); r != nil {
-			b.Fatal("benchmark fatal BenchmarkFromLatLonF")
-		}
-	}()
-	lat := 64.83778
-	lon := -147.71639
-	for i := 0; i < b.N; i++ {
-		UTM.FromLatLonF(lat, lon)
 	}
 }
